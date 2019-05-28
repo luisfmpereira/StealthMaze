75c929bc31824cc05f563c43597ebbe575c5b18e
Luis Filipe Modesto Pereira <luisf.modestop@gmail.com>
2019-05-27 23:47:56 -0300

    starting player shoot mechanic

---
+++ Assets/_Game/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+
+    public Transform muzzle;
+    public Rigidbody bulletPrefab;
+    public float bulletInitialForce = 10;
+    public float cooldown;
+    float maxCooldown;
+
+    void Update()
+    {
+        if (cooldown <= 0 && Input.GetMouseButtonDown(0))
+            Shoot();
+        else
+            cooldown -= Time.deltaTime;
+
+    }
+
+
+
+    void Shoot()
+    {
+        Rigidbody b = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation) as Rigidbody;
+        b.AddForce(muzzle.forward * bulletInitialForce);
+    }
+
+
+}
