20828ca30a976052e909386626c777de5260d63c
Luis Filipe Modesto Pereira <luisf.modestop@gmail.com>
2019-06-10 23:47:43 -0300

    fix camera clipping through walls

--- Assets/_Game/Scripts/Player/ThirdPersonCamera.cs
+++ Assets/_Game/Scripts/Player/ThirdPersonCamera.cs
@@ -8,13 +8,12 @@ public class ThirdPersonCamera : MonoBehaviour
     public bool lockCursor;
     public float mouseSensitivity = 10;
     public Transform target;
-    public float dstFromTarget = 5;
+    public float dstFromTarget;
     public Vector2 pitchMinMax = new Vector2(-20, 85);
 
     public float rotationSmoothTime = .12f;
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
-
     float yaw;
     float pitch;
 
@@ -37,7 +36,7 @@ public class ThirdPersonCamera : MonoBehaviour
         currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
         transform.eulerAngles = currentRotation;
 
-        transform.position = target.position - transform.forward * dstFromTarget;
+        //transform.position = target.position - transform.forward * dstFromTarget;
 
     }
 
---
+++ Assets/_Game/Scripts/PlayerCamera/CameraFollow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField] private Transform playerBody;
+	public float CameraMoveSpeed = 120.0f;
+	public GameObject CameraFollowObj;
+	Vector3 FollowPOS;
+	public float clampAngle = 80.0f;
+	public float inputSensitivity = 150.0f;
+	public GameObject CameraObj;
+	public GameObject PlayerObj;
+	public float camDistanceXToPlayer;
+	public float camDistanceYToPlayer;
+	public float camDistanceZToPlayer;
+	public float mouseX;
+	public float mouseY;
+	public float smoothX;
+	public float smoothY;
+	private float rotY = 0.0f;
+	private float rotX = 0.0f;
+
+	private void Awake()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// We setup the rotation of the sticks here
+		mouseX = Input.GetAxis ("Mouse X") * inputSensitivity * Time.deltaTime;
+		mouseY = Input.GetAxis ("Mouse Y") * -1 * inputSensitivity * Time.deltaTime;
+
+		rotY += mouseY ;
+		rotX += mouseX ; 
+
+		 rotY = Mathf.Clamp (rotY, -clampAngle, clampAngle);
+
+		Quaternion localRotation = Quaternion.Euler (rotY, rotX, 0.0f);
+		transform.rotation = localRotation;
+		//playerBody.Rotate(Vector3.up * mouseX);
+
+	}
+
+	void LateUpdate () {
+		CameraUpdater ();
+	}
+
+	void CameraUpdater() {
+		// set the target object to follow
+		Transform target = CameraFollowObj.transform;
+
+		//move towards the game object that is the target
+		float step = CameraMoveSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+}
